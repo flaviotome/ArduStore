c22cac043bc764078a6561408abac02d9262ec31
Flávio Tomé <flaviotome5@gmail.com>
2022-08-24 16:10:21 -0300

    implementacao do put e delete no backend; conexão entre o front-end e back end; lista de produtos carregada do banco de dados

--- api/Routes/productsRoutes.js
+++ api/Routes/productsRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import ProductController from "../Controllers/productController.js"
+import ProductController from "../Controllers/productController.js";
 
 const router = express.Router();
 
 router
-  .get("/products", ProductController.listProducts)
+  .get("/products", ProductController.getProducts)
+  .get("/products/:id", ProductController.getProductById)
+  .post("/products", ProductController.postProduct)
+  .put("/products/:id", ProductController.updateProduct)
+  .delete("/products/:id", ProductController.deleteProduct);
 export default router;
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,72 +7,14 @@ import { Product } from '../models/product';
   providedIn: 'root',
 })
 export class ProductService {
-
-
-  productList = [
+  listaDePRodutos: Product[] = [
     {
-      productId: '1',
-      productName: 'Arduino Uno',
-      productPrice: '70',
-      productDescription:
-        "Arduino Uno is a microcontroller board based on the ATmega328P (datasheet). It has 14 digital input/output pins (of which 6 can be used as PWM outputs), 6 analog inputs, a 16 MHz ceramic resonator (CSTCE16M0V53-R0), a USB connection, a power jack, an ICSP header and a reset button. It contains everything needed to support the microcontroller; simply connect it to a computer with a USB cable or power it with a AC-to-DC adapter or battery to get started.. You can tinker with your Uno without worrying too much about doing something wrong, worst case scenario you can replace the chip for a few dollars and start over again 'Uno' means one in Italian and was chosen to mark the release of Arduino Software (IDE) 1.0. The Uno board and version 1.0 of Arduino Software (IDE) were the reference versions of Arduino, now evolved to newer releases. The Uno board is the first in a series of USB Arduino boards, and the reference model for the Arduino platform; for an extensive list of current, past or outdated boards see the Arduino index of boards.",
-      productType: 'board',
-      productImg: '/assets/carrossel/arduino_uno.jpg',
-    },
-    {
-      productId: '2',
-      productName: 'Arduino Mega',
-      productPrice: '140',
-      productDescription:
-        'The 8-bit board with 54 digital pins, 16 analog inputs, and 4 serial ports.',
-      productType: 'board',
-      productImg: '/assets/carrossel/arduino_mega.jpg',
-    },
-    {
-      productId: '3',
-      productName: 'Arduino Nano',
-      productPrice: '50',
-      productDescription:
-        "The Arduino Nano is Arduino's classic breadboard friendly designed board with the smallest dimensions. The Arduino Nano comes with pin headers that allow for an easy attachment onto a breadboard and features a Mini-B USB connector.",
-      productType: 'board',
-      productImg: '/assets/carrossel/arduino_nano.jpg',
-    },
-
-    {
-      productId: '5',
-      productName: 'Sensor DHT 11',
-      productPrice: '140',
-      productDescription:
-        'The 8-bit board with 54 digital pins, 16 analog inputs, and 4 serial ports.',
-      productType: 'sensor',
-      productImg: '/assets/products-images/ultrassonico.jpg',
-    },
-    {
-      productId: '6',
-      productName: 'Sensor de Movimento',
-      productPrice: '50',
-      productDescription:
-        "The Arduino Nano is Arduino's classic breadboard friendly designed board with the smallest dimensions. The Arduino Nano comes with pin headers that allow for an easy attachment onto a breadboard and features a Mini-B USB connector.",
-      productType: 'sensor',
-      productImg: '/assets/products-images/movimento.jpg',
-    },
-    {
-      productId: '7',
-      productName: 'Sensor de Temperatura',
-      productPrice: '50',
-      productDescription:
-        "The Arduino Nano is Arduino's classic breadboard friendly designed board with the smallest dimensions. The Arduino Nano comes with pin headers that allow for an easy attachment onto a breadboard and features a Mini-B USB connector.",
-      productType: 'sensor',
-      productImg: '/assets/products-images/temperatura.jpg',
-    },
-    {
-      productId: '8',
-      productName: 'Sensor de Monóxido de Carbono',
-      productPrice: '50',
-      productDescription:
-        "The Arduino Nano is Arduino's classic breadboard friendly designed board with the smallest dimensions. The Arduino Nano comes with pin headers that allow for an easy attachment onto a breadboard and features a Mini-B USB connector.",
-      productType: 'sensor',
-      productImg: '/assets/products-images/carbono.jpg',
+      id: '',
+      productName: '',
+      productPrice: 0,
+      productDescription: '',
+      productType: '',
+      productImg: '',
     },
   ];
 
@@ -86,41 +28,41 @@ export class ProductService {
   //used to inform wich type of product list will be opened in the product-list Page
   productType = '';
 
-  url = "http://localhost:3000/products"
+  url = 'http://localhost:3000/products';
 
   constructor(private httpClient: HttpClient) {
     this.boardList = this.productListTypeFilter('board');
     this.sensorList = this.productListTypeFilter('sensor');
+    this.retornaProdutos().subscribe((products) => {
+      this.listaDePRodutos = products;
+    });
   }
 
   getProductFromId(id: any) {
     let result: any;
-    result = this.productList.find((element) => element.productId == id);
+    result = this.listaDePRodutos.find((element) => element.id == id);
 
     return result;
   }
 
   //filters the productList by the type
   productListTypeFilter(type: string) {
-    return this.productList.filter((p) => p.productType == type);
+    return this.listaDePRodutos.filter((p) => p.productType == type);
   }
 
   getProductList() {
-    return this.productList;
+    return this.listaDePRodutos;
   }
 
   getBoardList() {
     return this.productListTypeFilter('board');
   }
+
   getSensorList() {
     return this.productListTypeFilter('sensor');
   }
 
-  retornaProdutos(): Observable<Product[]>{
-
-    return this.httpClient.get<Product[]>(this.url)
-
+  retornaProdutos(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.url);
   }
-
-
 }
